67d963e2cb3ebe5e13cd069eddf154c43288d301
Lucio Batistella <lucio.batistella@gmail.com>
2025-08-22 15:27:47 -0300

    feat: change bg-background body

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="min-h-screen bg-white text-gray-900 bg-gray-50">
+  <div class="min-h-screen bg-gray-50 text-gray-900 bg-gray-50">
     <RouterView />
   </div>
 </template>
